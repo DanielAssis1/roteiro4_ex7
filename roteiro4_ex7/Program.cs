8544c8e6bbe7fb5fa38e454dc0ad68ff595c03af
DanielAssis1 <43318923+DanielAssis1@users.noreply.github.com>
2018-10-01 14:42:27 -0300

    att1

--- roteiro4_ex7/Program.cs
+++ roteiro4_ex7/Program.cs
@@ -10,6 +10,54 @@ namespace roteiro4_ex7
     {
         static void Main(string[] args)
         {
+            int idade, i, fx1=0, fx2=0, fx3=0, fx4=0, fx5=0;
+
+            for (i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Digite a idade: ");
+                idade = int.Parse(Console.ReadLine());
+                if (idade < 0)
+                {
+                    Console.WriteLine("A idade digitada é inválida.");
+                    i--;
+                }
+                else
+                {
+                    if (idade > 60)
+                    {
+                        fx5++;
+
+                    }
+                    else if (idade > 45)
+                    {
+                        fx4++;
+                    }
+                    else if(idade > 30)
+                    {
+                        fx3++;
+                    }
+                    else if (idade > 15)
+                    {
+                        fx2++;
+                    }
+                    else if(idade >= 0)
+                    {
+                        fx1++;
+                    }
+
+
+
+
+                }
+            }
+            Console.WriteLine("A 1ª Faixa Etária possui: " + fx1 + " pessoas.\n");
+            Console.WriteLine("A 2ª Faixa Etária possui: " + fx2 + " pessoas.\n");
+            Console.WriteLine("A 3ª Faixa Etária possui: " + fx3 + " pessoas.\n");
+            Console.WriteLine("A 4ª Faixa Etária possui: " + fx4 + " pessoas.\n");
+            Console.WriteLine("A 5ª Faixa Etária possui: " + fx5 + " pessoas.\n");
+            Console.WriteLine("A porcentagem de pessoas na 1ª faixa etária é: "+(fx1*100)/i+"%.");
+            Console.WriteLine("A porcentagem de pessoas na 5ª faixa etária é: " + (fx5 * 100) / i + "%.");
+            Console.ReadKey();
         }
     }
 }
